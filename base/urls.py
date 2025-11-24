02af83d81d2401a54aae5f6310277264c1f2d1f7
MdRaselHosen <raselh280@gmail.com>
2025-11-25 02:09:07 +0600

    create form

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room", views.room, name='room'),
-    path("room_page/<str:pk>", views.room_page, name="room-page"),
+    # path("room_page/<str:pk>", views.room_page, name="room-page"),
+    path("createRoom", views.createRoom, name='create-room'),
 ]
