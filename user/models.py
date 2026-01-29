8fec0125ff9a3767954be7c4e72bad5bde3d008a
MdRaselHosen <raselh280@gmail.com>
2026-01-29 19:05:46 +0600

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class user(models.Model):
+    username = models.CharField(max_length=15, null=False)
+    email = models.EmailField()
+    password = models.CharField(max_length=20, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.email
